594a5fa6a34e8291ec0a8dd67841d79f9d4a0f7f
redka <thisredoned@gmail.com>
2019-01-17 01:23:22 +0000

    fix --env based webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -4,100 +4,109 @@ const path = require('path');
 const PlayCanvasWebpackPlugin = require('playcanvas-webpack-plugin');
 const configuration = require('./config.json');
 
-const config = {
-  entry: {
-    main: './src/main.coffee'
-  },
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: '[name].build.js'
-  },
-  resolve: {
-    extensions: ['.js', '.coffee']
-  },
-  plugins: [
-    new PlayCanvasWebpackPlugin({
-      skipUpload: process.env.NODE_ENV !== "production" || !configuration.bearer || configuration.bearer.length != 32,
-      bearer: configuration.bearer,
-      project: configuration.projectId,
-      files: configuration.files || {
-        "main.build.js": {
-          path: "main.build.js",
-          assetId: configuration.assetId
-        }
-      }
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.coffee/,
-        exclude: /node_modules/,
-        loader: 'coffee-loader'
-      },
-      {
-        test: /\.glsl$/,
-        loader: 'raw-loader'
-      }
-    ]
-  }
-};
 
-let extraConfig = {}
+module.exports = function(env) {
+  if (!env) env = { development: true };
 
-if (process.env.NODE_ENV === 'production') {
-
-  extraConfig = {
-    mode: 'production',
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-            }
-          }
-        }
-      ]
-    }
-  };
-} else {
-
-  extraConfig = {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-      port: 8081,
-      disableHostCheck: true,
-      contentBase: './build',
-      hot: true,
-      overlay: true,
-      inline: true,
-      open: false,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-        "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
-      }
+  const config = {
+    entry: {
+      main: './src/main.coffee'
     },
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: '[name].build.js'
+    },
+    resolve: {
+      extensions: ['.js', '.coffee']
+    },
+    plugins: [
+      new PlayCanvasWebpackPlugin({
+        skipUpload: env.development || !configuration.bearer || configuration.bearer.length != 32,
+        bearer: configuration.bearer,
+        project: configuration.projectId,
+        files: configuration.files || {
+          "main.build.js": {
+            path: "main.build.js",
+            assetId: configuration.assetId
+          }
+        }
+      }),
+      new webpack.EnvironmentPlugin({
+        NODE_ENV: env.production ? 'production' : 'development'
+      })
+    ],
     module: {
       rules: [
         {
-          test: /\.js$/,
+          test: /\.coffee/,
           exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env']
-            }
-          }
+          loader: 'coffee-loader'
+        },
+        {
+          test: /\.glsl$/,
+          loader: 'raw-loader'
         }
       ]
     }
   };
-}
 
-module.exports = merge.smart(config, extraConfig);
+  let extraConfig = {}
+
+  if (env.production) {
+
+    extraConfig = {
+      mode: 'production',
+      devtool: 'cheap-source-map',
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env'],
+              }
+            }
+          }
+        ]
+      }
+    };
+  } else {
+
+    extraConfig = {
+      mode: 'development',
+      devtool: 'inline-source-map',
+      devServer: {
+        port: 8081,
+        disableHostCheck: true,
+        contentBase: './build',
+        hot: true,
+        overlay: true,
+        inline: true,
+        open: false,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+          "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+        }
+      },
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          }
+        ]
+      }
+    };
+  }
+
+  return merge.smart(config, extraConfig);
+}
